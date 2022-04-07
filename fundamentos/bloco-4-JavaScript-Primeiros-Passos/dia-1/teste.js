00f018dc1e56679c5445328da69e30e68b973bd5
João Gallo <joaogallop@gmail.com>
2022-04-07 11:43:46 -0300

    Adiciona dois primeiros exercicios

--- fundamentos/bloco-4-JavaScript-Primeiros-Passos/dia-1/teste.js
+++ fundamentos/bloco-4-JavaScript-Primeiros-Passos/dia-1/teste.js
@@ -1,6 +1,30 @@
-const myName = "joao"
-const birthCity = "aoijaoi"
-const birthYear = "2030"
-console.log (myName)
-console.log (birthCity)
-console.log (birthYear)
+const a = 9845
+const b = 8978
+// adição
+console.log(a + b)
+
+// subtração
+console.log(a - b)
+
+// multiplicação
+console.log(a * b)
+
+// divisão
+console.log (a / b)
+
+// módulo
+console.log (a % b)
+// separando
+console.log ("      ")
+
+// Programa para retornar o maior numero
+const n1 = 1235643
+const n2 = 9102810
+
+
+if(n1 > n2){
+    console.log(n1 + " é maior q " + n2)
+}
+else if (n2 > n1){
+    console.log (n2 + " é maior q " + n1)
+}
