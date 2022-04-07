57b0b5ee18119574b1275d3e966b6e41e57c7294
João Gallo <joaogallop@gmail.com>
2022-04-07 18:20:40 -0300

    Adiciona calculadora de Imposto de Renda e INSS e outros exercicios

--- fundamentos/bloco-4-JavaScript-Primeiros-Passos/dia-1/teste.js
+++ fundamentos/bloco-4-JavaScript-Primeiros-Passos/dia-1/teste.js
@@ -92,3 +92,197 @@ console.log("faz o L")
 // separando
 console.log ("      ")
 // separando
+
+//Porcentagem
+const nots = 2112
+if (nots >= 90 && nots < 100){
+    console.log ("A")
+}
+else if ( nots >= 80 && nots < 90){
+    console.log ("B")
+}
+else if (nots >= 70 && nots < 80){
+    console.log ("C")
+}
+else if (nots >= 60 && nots < 70){
+    console.log ("D")
+}
+else if (nots >= 50 && nots < 60){
+    console.log ("E")
+}
+else if (nots < 50){
+    console.log ("F")
+}
+else if (nots > 100 || nots < 0){
+    console.log ("Erro")
+}
+
+// separando
+console.log ("      ")
+// separando
+
+// Parolimpa
+const nu1 = 212
+const nu2 = 398
+const nu3 = 93
+if(nu1 % 2 == 0 || nu2 % 2 == 0 || nu3 % 2 == 0){
+    console.log (true)
+}
+else{
+    console.log (false)
+}
+
+// separando
+console.log ("      ")
+// separando
+
+// Parolimpa
+
+if(nu1 % 2 == 0 || nu2 % 2 == 0 || nu3 % 2 == 0){
+    console.log (false)
+}
+else{
+    console.log (true)
+}
+
+// separando
+console.log ("      ")
+// separando
+
+const valor1 = 102
+const valor2 = 1921
+imposto = (valor2 * 20 / 100)
+let custo = (imposto + valor1)
+lucro = (valor2 - custo)
+console.log (lucro * 1000)
+
+// separando
+console.log ("      ")
+// separando
+
+const salario = 30000
+console.log ("Sendo o salário " + salario)
+
+if (salario <= 1556.94){
+    let inss1 = (salario * 8 / 100)
+    let seminss = (salario - inss1)
+    console.log ("o inss será " + inss1)
+        if (seminss < 1903.98){
+            console.log("nao paga ir")
+        }
+        else if(seminss >= 1903.99 && seminss <= 2826.65){
+            let imRe = ((seminss * 7.5 / 100) - 142.80)
+            console.log ("o imposto será " + imRe)
+            console.log ("o salário liquido será " + (seminss - imRe)) 
+        }
+        else if(seminss >= 2826.66 && seminss <= 3751.05){
+            let imRe = ((seminss * 15 / 100) - 358.80)
+            console.log ("o imposto será " + imRe)
+            console.log ("o salário liquido será " + (seminss - imRe))
+        }
+        else if(seminss >= 3751.06 && seminss <= 4664.68){
+            let imRe = ((seminss * 22.5 / 100) - 636.13)
+            console.log ("o imposto será " + imRe)
+            console.log ("o salário liquido será " + (seminss - imRe)) 
+        }
+        else if(seminss > 4664.68){
+            let imRe = ((seminss * 27.5 / 100) - 869.36)
+            console.log ("o imposto será " + imRe)
+            console.log ("o salário liquido será " + (seminss - imRe))
+
+        }
+    
+}
+else if (salario > 1556.95 && salario <=2594.92){
+    let inss1 = (salario * 9 /100)
+    seminss = (salario - inss1)
+    console.log("o inss será " + inss1)
+        if (seminss < 1903.98){
+            console.log("nao paga ir")
+        }
+        else if(seminss >= 1903.99 && seminss <= 2826.65){
+            let imRe = ((seminss * 7.5 / 100) - 142.80)
+            console.log ("o imposto será " + imRe)
+            console.log ("o salário liquido será " + (seminss - imRe)) 
+        }
+        else if(seminss >= 2826.66 && seminss <= 3751.05){
+            let imRe = ((seminss * 15 / 100) - 358.80)
+            console.log ("o imposto será " + imRe)
+            console.log ("o salário liquido será " + (seminss - imRe))
+        }
+        else if(seminss >= 3751.06 && seminss <= 4664.68){
+            let imRe = ((seminss * 22.5 / 100) - 636.13)
+            console.log ("o imposto será " + imRe)
+            console.log ("o salário liquido será " + (seminss - imRe)) 
+        }
+        else if(seminss > 4664.68){
+            let imRe = ((seminss * 27.5 / 100) - 869.36)
+            console.log ("o imposto será " + imRe)
+            console.log ("o salário liquido será " + (seminss - imRe))
+
+        }
+}
+else if (salario > 2594.92 && salario <=5189.82){
+    let inss1 = (salario * 11 /100)
+    console.log("O inss será " + inss1)
+    seminss = (salario - inss1)
+        if (seminss < 1903.98){
+            console.log("nao paga ir")
+        }
+        else if(seminss >= 1903.99 && seminss <= 2826.65){
+            let imRe = ((seminss * 7.5 / 100) - 142.80)
+            console.log ("o imposto será " + imRe)
+            console.log ("o salário liquido será " + (seminss - imRe)) 
+        }
+        else if(seminss >= 2826.66 && seminss <= 3751.05){
+            let imRe = ((seminss * 15 / 100) - 358.80)
+            console.log ("o imposto será " + imRe)
+            console.log ("o salário liquido será " + (seminss - imRe))
+        }
+        else if(seminss >= 3751.06 && seminss <= 4664.68){
+            let imRe = ((seminss * 22.5 / 100) - 636.13)
+            console.log ("o imposto será " + imRe)
+            console.log ("o salário liquido será " + (seminss - imRe)) 
+        }
+        else if(seminss > 4664.68){
+            let imRe = ((seminss * 27.5 / 100) - 869.36)
+            console.log ("o imposto será " + imRe)
+            console.log ("o salário liquido será " + (seminss - imRe))
+
+        }
+}
+
+else if (salario > 5189.82){
+    inss1 = 570.88
+    seminss = (salario - inss1)
+    console.log("O INSS será " + inss1)
+        if (seminss < 1903.98){
+            console.log("nao paga ir")
+        }
+        else if(seminss >= 1903.99 && seminss <= 2826.65){
+            let imRe = ((seminss * 7.5 / 100) - 142.80)
+            console.log ("o imposto será " + imRe)
+            console.log ("o salário liquido será " + (seminss - imRe)) 
+        }
+        else if(seminss >= 2826.66 && seminss <= 3751.05){
+            let imRe = ((seminss * 15 / 100) - 358.80)
+            console.log ("o imposto será " + imRe)
+            console.log ("o salário liquido será " + (seminss - imRe))
+        }
+        else if(seminss >= 3751.06 && seminss <= 4664.68){
+            let imRe = ((seminss * 22.5 / 100) - 636.13)
+            console.log ("o imposto será " + imRe)
+            console.log ("o salário liquido será " + (seminss - imRe)) 
+        }
+        else if(seminss > 4664.68){
+            let imRe = ((seminss * 27.5 / 100) - 869.36)
+            console.log ("o imposto será " + imRe)
+            console.log ("o salário liquido será " + (seminss - imRe))
+
+        }
+}
+
+
+
+
+
---
+++ fundamentos/bloco-4-JavaScript-Primeiros-Passos/ra.js
@@ -0,0 +1,16 @@
+let e = 200
+let positivo = e > 0
+let negativo = e < 0
+let zero = e == 0
+switch (e) {
+  case e > 0:
+      console.log('positivo');
+      break;
+  case e < 0:
+      console.log('negativo');
+      break;
+  case e == 0:
+      console.log('zero');
+      break;
+}
+    
