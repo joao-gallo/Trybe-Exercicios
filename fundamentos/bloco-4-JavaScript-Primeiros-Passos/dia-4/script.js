a8997c1088cefdc82bbd4f629f4bb8dafd85859a
João Gallo <joaogallop@gmail.com>
2022-04-11 18:19:47 -0300

    Adiciona mais exercicios

--- fundamentos/bloco-4-JavaScript-Primeiros-Passos/dia-4/script.js
+++ fundamentos/bloco-4-JavaScript-Primeiros-Passos/dia-4/script.js
@@ -3,6 +3,28 @@ let info = {
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
   };
+// separando
+console.log ('--------------')
+// separando
+
   console.log('Bem vinda, ' + info['personagem'])
+// separando
+console.log ('--------------')
+// separando
+
   info.recorrente = 'Sim'
   console.log (info)
+// separando
+console.log ('--------------')
+// separando
+
+for (let key in info){
+    console.log (key)
+}
+// separando
+console.log ('--------------')
+// separando
+
+for(key in info) {
+    console.log(info[key])
+}
