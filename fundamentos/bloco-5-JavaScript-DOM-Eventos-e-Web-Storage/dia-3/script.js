82f79698d629c0a8e6d6fda8e2c2818b464c6631
João Gallo <joaogallop@gmail.com>
2022-04-20 12:07:52 -0300

    exercicio 1

---
+++ fundamentos/bloco-5-JavaScript-DOM-Eventos-e-Web-Storage/dia-3/script.js
@@ -0,0 +1,48 @@
+function createDaysOfTheWeek() {
+    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+    const weekDaysList = document.querySelector('.week-days');
+  
+    for (let index = 0; index < weekDays.length; index += 1) {
+      const days = weekDays[index];
+      const dayListItem = document.createElement('li');
+      dayListItem.innerHTML = days;
+  
+      weekDaysList.appendChild(dayListItem);
+    };
+  };
+  
+  createDaysOfTheWeek();
+
+  
+  let dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  function daysOnArray(){
+    let getDays = document.querySelector('#days');
+
+    for (let i = 0; i < dezDaysList.length; i += 1){
+        let day = dezDaysList[i]
+        let dayItem = document.createElement('li');
+        
+        
+        if(day === 24 || day === 31){
+            day.className = "day holiday"
+            dayItem.innerHTML = day
+            getDays.appendChild(dayItem)
+        }
+        if (day === 4 || day === 11 || day === 18){
+            day.className = "day friday"
+            dayItem.innerHTML = day
+            getDays.appendChild(dayItem)
+        }
+        if (day === 25){
+            day.className = "day friday holiday"
+            dayItem.innerHTML = day
+            getDays.appendChild(dayItem)
+        }else{
+            dayItem.innerHTML = day
+            dayItem.className = 'day'
+            getDays.appendChild(dayItem)
+        }
+    }
+
+  };
+  daysOnArray();
