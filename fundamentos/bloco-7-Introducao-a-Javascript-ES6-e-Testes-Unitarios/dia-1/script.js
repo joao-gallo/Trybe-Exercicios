cb9685e0131d1cf156ebdcc5f0ccd9aabc2b022c
João Gallo <joaogallop@gmail.com>
2022-05-06 17:38:59 -0300

    Adiciona parte 1

---
+++ fundamentos/bloco-7-Introducao-a-Javascript-ES6-e-Testes-Unitarios/dia-1/script.js
@@ -0,0 +1,39 @@
+// function testingScope(escopo) {
+//     if (escopo === true) {
+//       var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//       ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+//       console.log(ifScope);
+//     } else {
+//       var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//       console.log(elseScope);
+//     }
+//     console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+//   }
+
+//   testingScope(true);
+
+//   const test = (escopo) => {
+//     if (escopo === true) {
+//     var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//     ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+//     console.log(ifScope);
+//   } else {
+//     var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//     console.log(elseScope);
+//   }
+// }
+// test(true)
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+// Seu código aqui.
+const cresc = () => {
+    oddsAndEvens[0] = 2;
+    oddsAndEvens[1] = 3;
+    oddsAndEvens[2] = 4;
+    oddsAndEvens[3] = 7;
+    oddsAndEvens[4] = 10;
+    oddsAndEvens[5] = 13;
+return oddsAndEvens
+}
+
+console.log(`Os números ${cresc()} se encontram ordenados de forma crescente!`); // será necessário alterar essa linha 😉
