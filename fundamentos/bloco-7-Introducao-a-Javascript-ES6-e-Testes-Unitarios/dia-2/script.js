dcb81760951a56f3b0a70ded2a8be95888b2c5a9
João Gallo <joaogallop@gmail.com>
2022-05-09 19:09:08 -0300

    Adiciona exercicio 1

--- fundamentos/bloco-7-Introducao-a-Javascript-ES6-e-Testes-Unitarios/dia-2/script.js
+++ fundamentos/bloco-7-Introducao-a-Javascript-ES6-e-Testes-Unitarios/dia-2/script.js
@@ -1,12 +1,26 @@
+function verify(value1, value2){
+    if (value1 === '' || value2 === ''){
+        throw new Error ('Valores invalidos')
+    }
+}
+
 function sum() {
+    try{
     const value1 = document.getElementById('value1').value;
     const value2 = document.getElementById('value2').value;
+    verify(value1, value2);
     const result = parseInt(value1) + parseInt(value2);
-    document.getElementById('result').innerHTML = `Resultado: ${result}`;
+    document.getElementById('result').innerHTML = `Resultado:${result}`;
     document.getElementById('value1').value = '';
     document.getElementById('value2').value = '';
-  }
-  window.onload = () => {
+    }
+catch (error){
+    document.getElementById('result').innerHTML = `Erro: ${error.message}`;
+    document.getElementById('value1').value = '';
+    document.getElementById('value2').value = '';
+}
+}
+ window.onload = () => {
     const button = document.getElementById('button');
     button.addEventListener('click', sum);
   }
