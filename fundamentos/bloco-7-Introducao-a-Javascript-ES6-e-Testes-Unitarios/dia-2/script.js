ab23214ca009b16bbf9b00860a60ee5112a03151
João Gallo <joaogallop@gmail.com>
2022-05-09 19:16:57 -0300

    Adiciona exercicio o resto

--- fundamentos/bloco-7-Introducao-a-Javascript-ES6-e-Testes-Unitarios/dia-2/script.js
+++ fundamentos/bloco-7-Introducao-a-Javascript-ES6-e-Testes-Unitarios/dia-2/script.js
@@ -3,19 +3,29 @@ function verify(value1, value2){
         throw new Error ('Valores invalidos')
     }
 }
+function isN(value1, value2){
+    if (isNaN(value1) || isNaN(value2)){
+        throw new Error ('Valores não-numericos')
+    }
+}
 
 function sum() {
     try{
     const value1 = document.getElementById('value1').value;
     const value2 = document.getElementById('value2').value;
     verify(value1, value2);
+    isN(value1, value2)
     const result = parseInt(value1) + parseInt(value2);
     document.getElementById('result').innerHTML = `Resultado:${result}`;
-    document.getElementById('value1').value = '';
-    document.getElementById('value2').value = '';
+    // document.getElementById('value1').value = '';
+    // document.getElementById('value2').value = '';
     }
 catch (error){
     document.getElementById('result').innerHTML = `Erro: ${error.message}`;
+    // document.getElementById('value1').value = '';
+    // document.getElementById('value2').value = '';
+}
+finally{
     document.getElementById('value1').value = '';
     document.getElementById('value2').value = '';
 }
